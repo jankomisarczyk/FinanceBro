5af25bfe0544aa703fc0258d82ca6a9e4ce13959
Jan Komisarczyk <jan.komisarczyk@gmail.com>
2023-11-17 17:04:52 +0100

    bs

--- src/plugins/get_balance_sheet_to_csv.py
+++ src/plugins/get_balance_sheet_to_csv.py
@@ -4,15 +4,15 @@ from src.interns.step import Execution
 from src.llmopenai import Argument
 from src.plugins.plugin import Plugin
 
-PLUGIN_NAME = "get_income_statement_to_csv"
-PLUGIN_DESCRIPTION = "Writes income statement for ticker symbol to a csv file."
+PLUGIN_NAME = "get_balance_sheet_to_csv"
+PLUGIN_DESCRIPTION = "Writes balance sheet for ticker symbol to a csv file."
 ARGS_SCHEMA = {
     "ticker": Argument(type="string", description="Stock ticker symbol"),
-    "filename": Argument(type="string", description="Name of csv file to which income statement will be written")
+    "filename": Argument(type="string", description="Name of csv file to which balance sheet will be written")
 }
     
 
-class GetIncomeStatementToCsv(Plugin):
+class GetBalanceSheetToCsv(Plugin):
     name = PLUGIN_NAME
     description = PLUGIN_DESCRIPTION
     args_schema = ARGS_SCHEMA
@@ -24,20 +24,20 @@ class GetIncomeStatementToCsv(Plugin):
         try:
             # just safety measure, if passed filename=None or not csv
             if not filename:
-                filename = f"{ticker}_income_statement.csv"
+                filename = f"{ticker}_balance_sheet.csv"
             else:
                 if not filename[-4:] == ".csv":
                     filename += ".csv"
             
             stock =  yfinance.Ticker(ticker)
-            dataframe = stock.income_stmt.transpose()
+            dataframe = stock.balance_sheet.transpose()
             dataframe.index.name = "Date"
-            # TODO what columns do I really need for analysis columns=["EBITDA" ....]
+            # TODO what columns do I really need for analysis 
             dataframe.to_csv(filename, encoding='utf-8')
             return Execution(
-                observation=f"Income Statement was successfully written to {filename}."
+                observation=f"Balance Sheet was successfully written to {filename}."
             )
         except Exception as e:
             return Execution(
-                observation=f"Error on execution of {GetIncomeStatementToCsv.name}: {e}"
+                observation=f"Error on execution of {GetBalanceSheetToCsv.name}: {e}"
             )
