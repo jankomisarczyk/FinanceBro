c6d1bee108054571fb6313f3f6cf3b765d99000a
Jan Komisarczyk <jan.komisarczyk@gmail.com>
2023-11-28 21:56:12 +0100

    Vector store

---
+++ src/interns/vector_store_manager_specialization.py
@@ -0,0 +1,42 @@
+from src.interns.specialization import Specialization
+from src.plugins.exit import Exit
+
+
+PLANNING_PROMPT_TEMPLATE = """As the AI Vector Store Manager, your role is to strategize and plan the execution of tasks efficiently and effectively. Avoid redundancy, such as unnecessary immediate verification of actions.
+
+# Functions
+## The AI Vector Store Manager can call only these functions:
+{functions}.
+
+Once the task has been completed, instruct the AI Vector Store Manager to call the `exit` function with all arguments to indicate the completion of the task.
+
+# Task
+## Your task, given by the human, is:
+{task}
+
+{history}
+{variables}
+{file_list}
+
+# Instructions
+##  Now, devise a concise and adaptable plan to guide the AI Vector Store Manager. Follow these guidelines:
+
+1. Ensure you interpret the execution history correctly while considering the order of execution. Avoid repetitive actions, e.g. if the same file has been read previously and the content hasn't changed.
+2. Regularly evaluate your progress towards the task goal. This includes checking the current state of the system against the task requirements and adjusting your strategy if necessary.
+3. If an error occurs (like 'File not found'), take a step back and analyze if it's an indicator of the next required action (like creating the file). Avoid getting stuck in loops by not repeating the action that caused the error without modifying the approach.
+4. Recognize when the task has been successfully completed according to the defined goal and exit conditions. If the task has been completed, instruct the AI Vector Store Manager to call the `exit` function.
+5. Determine the most efficient next action towards completing the task, considering your current information, requirements, and available functions.
+6. Direct the execution of the immediate next action using exactly one of the callable functions, making sure to skip any redundant actions that are already confirmed by the historical context.
+
+Provide a concise analysis of the past history, followed by an overview of your plan going forward, and end with one sentence describing the immediate next action to be taken."""
+
+class FileManager(Specialization):
+    NAME = "Vector Store Manager Agent"
+    DESCRIPTION = "Vector Store Manager Agent: Specializes at managing files and folders. It can perform wide verity of operations on files, including opening (viewing or printing), writing to a file, renaming, copying, moving, deleting and searching for files."
+    PLUGINS = {
+        "create_vector_store_from_pdfs": CreateVectorStoreFromPDFs,
+        "create_vector_store_from_links": CreateVectorStoreFromLinks,
+        "query_vector_store": QueryVectorStore,
+        "exit": Exit
+    }
+    planning_prompt_template = PLANNING_PROMPT_TEMPLATE
---
+++ src/plugins/create_vector_store_from_pdfs.py
@@ -0,0 +1,63 @@
+import pickle
+from typing import List
+
+from langchain.document_loaders import PyPDFLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+
+from src.interns.step import Execution
+from src.llmopenai import Argument, ItemsType
+from src.plugins.plugin import Plugin
+
+PLUGIN_NAME = "create_vector_store_from_pdfs"
+PLUGIN_DESCRIPTION = "Creates vector store from a list of .pdf files."
+ARGS_SCHEMA = {
+    "pdfs_list": Argument(type="array", items=ItemsType(type="string"), minItems=1, uniqueItems=True, description="A list with .pdf file names.")
+}
+    
+
+class CreateVectorStoreFromPDFs(Plugin):
+    name = PLUGIN_NAME
+    description = PLUGIN_DESCRIPTION
+    args_schema = ARGS_SCHEMA
+    required = ["pdfs_list"]
+    categories = ["Vector Store"]
+    
+    @staticmethod
+    async def arun(pdfs_list: List[str]) -> Execution:
+        try:
+            docs = []
+
+            for pdf in pdfs_list:
+                loader = PyPDFLoader(pdf)
+                sub_docs = loader.load()
+                for doc in sub_docs:
+                    doc.metadata["source"] = pdf
+                docs.extend(sub_docs)
+            
+            text_splitter = RecursiveCharacterTextSplitter(
+                chunk_size=1000,
+                chunk_overlap=200)
+            chunks = text_splitter.split_documents(docs)
+
+            for id, chunk in enumerate(chunks):
+                chunk.metadata["UID"] = id
+            embeddings = OpenAIEmbeddings
+            vectorstore = FAISS.from_documents(chunks, embeddings)
+            # first ten char of first pdf name
+            vs_name = f"vector_store_{pdfs_list[0][:10]}.pkl"
+            with open(vs_name, 'wb') as f:
+                pickle.dump(vectorstore, f)
+            
+            return Execution(
+                observation=f"Successfully created vector store under name {vs_name}."
+            )
+        except Exception as e:
+            if str(e) == "Couldn't find any active App!":
+                return Execution(
+                    observation="No Excel is opened and thus also active."
+                )
+            return Execution(
+                observation=f"Error on execution of {CreateVectorStoreFromPDFs.name}: {e}"
+            )
